1ec38449e70285f751afdf0448c53dec59a5381b
canow77 <cangelgin@hotmail.com>
2018-04-04 13:56:26 +0300

    update 2
    ok

---
+++ Assets/Oyuncular/Oyuncu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;//extra
+using UnityEngine.Networking;//extra
+
+public class Oyuncu : NetworkBehaviour {
+
+    private Vector3 girdiDegerleri;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        girdiDegerleri.x = CrossPlatformInputManager.GetAxis("Horizontal");
+        girdiDegerleri.y = 0f;
+        girdiDegerleri.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+        transform.Translate(girdiDegerleri);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<Camera>().enabled = true;
+    }
+}
