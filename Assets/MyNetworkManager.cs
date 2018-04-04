1b518c296476d7f8d2fd2027643c4dc9ecf7f65d
canow77 <cangelgin@hotmail.com>
2018-04-04 11:48:15 +0300

    Initial commit

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking; //ekstra
+
+public class MyNetworkManager : NetworkManager
+{
+    public void MyStartHost() // butonda çağıralacak metod
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Starting Host." );
+        StartHost();
+    }
+
+    public override void OnStartHost() 
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Host started." );
+    }
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " client start requested");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Clientsu IP ye bağlandı: " + conn.address);
+    }
+
+    void PrintDots()
+    {
+
+    }
+}
